aa67c392f0194dd52dbeaccb695683a511759dac
Vasanth <vasanthravi66@gmail.com>
2026-06-10 09:32:08 +0530

    change nodermiler

--- config/nodemiler.js
+++ config/nodemiler.js
@@ -6,13 +6,19 @@ console.log("SMTP PASS:", process.env.SMTP_PASS);
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // IMPORTANT
+  port: 2525, // better for Render
+  secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
+
+export default transporter;
 
 export default transporter;
 
